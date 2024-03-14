1b9b2a1a87417886eafb0059d3382543c10d3856
Gergö Kadar <kadargergo20@gmail.com>
2024-03-14 13:47:28 +0100

    To be ting tilføjet

---
+++ untitled/src/BE/Events.java
@@ -0,0 +1,88 @@
+package BE;
+import java.time.LocalDateTime;
+
+public class Events {
+private int id;
+private String eventName;
+private String eventHost;
+private LocalDateTime eventStart;
+private LocalDateTime eventEnd;
+private String eventLocation;
+private String eventNotes;
+private String eventGuidance;
+
+    public Events(int id, String eventName, String eventHost, LocalDateTime eventStart, LocalDateTime eventEnd, String eventLocation, String eventNotes, String eventGuidance)
+    {
+        this.id = id;
+        this.eventName = eventName;
+        this.eventStart = eventStart;
+        this.eventEnd = eventEnd;
+        this.eventLocation = eventLocation;
+        this.eventNotes = eventNotes;
+        this.eventGuidance = eventGuidance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventHost() {
+        return eventHost;
+    }
+
+    public void setEventHost(String eventHost) {
+        this.eventHost = eventHost;
+    }
+
+    public LocalDateTime getEventStart() {
+        return eventStart;
+    }
+
+    public void setEventStart(LocalDateTime eventStart) {
+        this.eventStart = eventStart;
+    }
+
+    public LocalDateTime getEventEnd() {
+        return eventEnd;
+    }
+
+    public void setEventEnd(LocalDateTime eventEnd) {
+        this.eventEnd = eventEnd;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
+    }
+
+    public void setEventLocation(String eventLocation) {
+        this.eventLocation = eventLocation;
+    }
+
+    public String getEventNotes() {
+        return eventNotes;
+    }
+
+    public void setEventNotes(String eventNotes) {
+        this.eventNotes = eventNotes;
+    }
+
+    public String getEventGuidance() {
+        return eventGuidance;
+    }
+
+    public void setEventGuidance(String eventGuidance) {
+        this.eventGuidance = eventGuidance;
+    }
+}
---
+++ untitled/src/BE/Users.java
@@ -0,0 +1,50 @@
+package BE;
+
+public class Users {
+    private int id;
+    private String userType;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String userEmail;
+
+    public Users(int id, String userType, String firstName, String lastName, String userName, String password, String userEmail) {
+        this.id = id;
+        this.userType = userType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.userEmail = userEmail;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+}
