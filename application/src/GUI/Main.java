d6cdfd01e4a5dc5a5615b524bf3798a0555eff8b
Rune Krogh <runekrogh20@gmail.com>
2024-03-20 19:26:13 +0100

    Basic UI for event creator + Main

--- application/src/GUI/Main.java
+++ application/src/GUI/Main.java
@@ -1,7 +1,24 @@
 package GUI;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/eventCreatorView.fxml")));
+        primaryStage.setTitle("Event Creator View");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
