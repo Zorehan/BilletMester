bef48b6f87810f7116fef4c99852cc79e4b1137e
Rune Krogh <runekrogh20@gmail.com>
2024-03-22 01:34:19 +0100

    Update mainViewController.java

--- application/src/GUI/Controller/mainViewController.java
+++ application/src/GUI/Controller/mainViewController.java
@@ -19,30 +19,33 @@ public class mainViewController {
         Task<Pair<Parent, eventCreatorController>> task = new Task<>() {
             @Override
             protected Pair<Parent, eventCreatorController> call() throws IOException {
+                // Inside the call method, we define the asynchronous task to be executed
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/eventCreatorView.fxml"));
                 Parent parent = loader.load();
                 eventCreatorController controller = loader.getController();
-                return new Pair<>(parent, controller);
+                return new Pair<>(parent, controller); // Return the loaded Parent and controller as a Pair
             }
         };
 
+        // Define actions to be performed when the task succeeds
         task.setOnSucceeded(event -> {
             Pair<Parent, eventCreatorController> result = task.getValue();
             Parent p = result.getKey();
             eventCreatorController eventCreatorController = result.getValue();
-            borderPane.setLeft(p);
+            borderPane.setLeft(p); // Set the loaded Parent (scene) to the BorderPane
         });
 
+        // Define actions to be performed when the task fails
         task.setOnFailed(event -> {
             Throwable exception = task.getException();
             if (exception != null) {
-                exception.printStackTrace();
+                exception.printStackTrace(); // Print the stack trace of the exception
             }
         });
 
         // Start the task in a background thread
         Thread thread = new Thread(task);
-        thread.setDaemon(true);
+        thread.setDaemon(true); // Set the thread as daemon to allow the application to exit if the task is not complete
         thread.start();
     }
 }
