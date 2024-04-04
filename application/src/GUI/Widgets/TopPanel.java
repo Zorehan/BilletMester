30f6dd9816d01464c53e0f828f950acecede0dfd
NillasKA <kramernicklas@gmail.com>
2024-04-04 23:06:37 +0200

    Udvidelse til modular UI

--- application/src/GUI/Widgets/TopPanel.java
+++ application/src/GUI/Widgets/TopPanel.java
@@ -3,6 +3,7 @@ package GUI.Widgets;
 import GUI.Controller.EventManagement.eventCreatorController;
 import GUI.Controller.EventManagement.eventManagerController;
 import GUI.Controller.adminViewController;
+import GUI.Model.UserModel;
 import GUI.Model.ViewModel;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
@@ -24,16 +25,25 @@ import java.io.IOException;
  */
 public class TopPanel extends HBox {
     private ViewModel viewModel = ViewModel.getInstance();
-    private final int WIDTH = 400;
+    private UserModel userModel = UserModel.getInstance();
     public TopPanel() {
         this.setId("topPanel");
         this.getStylesheets().add("/GUI/Styling/BannerAndTopBar.css");
         initTopPanel();
 
-        System.out.println("init");
+        switch(userModel.getUser().getUserType()) {
+            case ADMIN:
+                initAdmin();
+                break;
+            case EVENTCOORDINATOR:
+                initEventCreator();
+                initEventManager();
+                break;
+        }
     }
 
     public void initTopPanel() {
+        this.setSpacing(5);
         this.setMaxWidth(1200);
         this.setMinWidth(800);
         this.setPrefWidth(1200);
@@ -76,9 +86,6 @@ public class TopPanel extends HBox {
         Button button = new Button("Event Manager");
         button.setId("userButton");
         button.setOnAction(this::clickEventManager);
-        this.setPrefWidth(600);
-        this.setLayoutX(WIDTH - 600);
-        this.setAlignment(Pos.CENTER_RIGHT);
         this.getChildren().addFirst(button);
     }
 
@@ -86,9 +93,6 @@ public class TopPanel extends HBox {
         Button button = new Button("Event Creator");
         button.setId("userButton");
         button.setOnAction(this::clickEventCreator);
-        this.setPrefWidth(600);
-        this.setLayoutX(WIDTH - 600);
-        this.setAlignment(Pos.CENTER_RIGHT);
         this.getChildren().addFirst(button);
     }
 
@@ -97,9 +101,6 @@ public class TopPanel extends HBox {
         Button button = new Button("Admin Panel");
         button.setId("userButton");
         button.setOnAction(this::clickAdminView);
-        this.setPrefWidth(500);
-        this.setLayoutX(WIDTH - 500);
-        this.setAlignment(Pos.CENTER_RIGHT);
         this.getChildren().addFirst(button);
     }
 
@@ -120,104 +121,39 @@ public class TopPanel extends HBox {
     }
 
     public void clickAdminView(ActionEvent actionEvent) {
-        // Load the new scene asynchronously
-        Task<Pair<Parent, adminViewController>> task = new Task<>() {
-            @Override
-            protected Pair<Parent, adminViewController> call() throws IOException {
-                // Inside the call method, we define the asynchronous task to be executed
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/adminView.fxml"));
-                Parent parent = loader.load();
-                adminViewController controller = loader.getController();
-                return new Pair<>(parent, controller); // Return the loaded Parent and controller as a Pair
-            }
-        };
-        // Define actions to be performed when the task succeeds
-        task.setOnSucceeded(event -> {
-            Pair<Parent, adminViewController> result = task.getValue();
-            Parent p = result.getKey();
-            adminViewController adminViewController = result.getValue();
-            viewModel.getBorderPane().setCenter(p); // Set the loaded Parent (scene) to the BorderPane
-        });
-        // Define actions to be performed when the task fails
-        task.setOnFailed(event -> {
-            Throwable exception = task.getException();
-            if (exception != null) {
-                exception.printStackTrace(); // Print the stack trace of the exception
-            }
-        });
-        // Start the task in a background thread
-        Thread thread = new Thread(task);
-        thread.setDaemon(true); // Set the thread as daemon to allow the application to exit if the task is not complete
-        thread.start();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/adminView.fxml"));
+            Parent userView = loader.load();
+
+            viewModel.getBorderPane().setCenter(userView);
+            viewModel.setTopBar();
+            viewModel.disableSidePanel();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void clickEventCreator(ActionEvent actionEvent) {
-        // Load the new scene asynchronously
-        Task<Pair<Parent, eventCreatorController>> task = new Task<>() {
-            @Override
-            protected Pair<Parent, eventCreatorController> call() throws IOException {
-                // Inside the call method, we define the asynchronous task to be executed
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/EventManagement/eventCreatorView.fxml"));
-                Parent parent = loader.load();
-                eventCreatorController controller = loader.getController();
-                return new Pair<>(parent, controller); // Return the loaded Parent and controller as a Pair
-            }
-        };
-
-        // Define actions to be performed when the task succeeds
-        task.setOnSucceeded(event -> {
-            Pair<Parent, eventCreatorController> result = task.getValue();
-            Parent p = result.getKey();
-            eventCreatorController eventManagerController = result.getValue();
-            viewModel.getBorderPane().setCenter(p); // Set the loaded Parent (scene) to the BorderPane
-        });
-
-        // Define actions to be performed when the task fails
-        task.setOnFailed(event -> {
-            Throwable exception = task.getException();
-            if (exception != null) {
-                exception.printStackTrace(); // Print the stack trace of the exception
-            }
-        });
-
-        // Start the task in a background thread
-        Thread thread = new Thread(task);
-        thread.setDaemon(true); // Set the thread as daemon to allow the application to exit if the task is not complete
-        thread.start();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/EventManagement/eventCreatorView.fxml"));
+            Parent userView = loader.load();
+            viewModel.getBorderPane().setCenter(userView);
+            viewModel.setTopBar();
+            viewModel.disableSidePanel();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void clickEventManager(ActionEvent actionEvent) {
-        // Load the new scene asynchronously
-        Task<Pair<Parent, eventManagerController>> task = new Task<>() {
-            @Override
-            protected Pair<Parent, eventManagerController> call() throws IOException {
-                // Inside the call method, we define the asynchronous task to be executed
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/EventManagement/eventManagerView.fxml"));
-                Parent parent = loader.load();
-                eventManagerController controller = loader.getController();
-                return new Pair<>(parent, controller); // Return the loaded Parent and controller as a Pair
-            }
-        };
-
-        // Define actions to be performed when the task succeeds
-        task.setOnSucceeded(event -> {
-            Pair<Parent, eventManagerController> result = task.getValue();
-            Parent p = result.getKey();
-            eventManagerController eventManagerController = result.getValue();
-            viewModel.getBorderPane().setCenter(p); // Set the loaded Parent (scene) to the BorderPane
-        });
-
-        // Define actions to be performed when the task fails
-        task.setOnFailed(event -> {
-            Throwable exception = task.getException();
-            if (exception != null) {
-                exception.printStackTrace(); // Print the stack trace of the exception
-            }
-        });
-
-        // Start the task in a background thread
-        Thread thread = new Thread(task);
-        thread.setDaemon(true); // Set the thread as daemon to allow the application to exit if the task is not complete
-        thread.start();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/EventManagement/eventManagerView.fxml"));
+            Parent userView = loader.load();
+            viewModel.getBorderPane().setCenter(userView);
+            viewModel.setTopBar();
+            viewModel.disableSidePanel();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
