2429119cb88c6d82738ae9ba5773b38750ad6afc
Nicklas Kramer <kramernicklas@gmail.com>
2024-04-15 21:34:04 +0200

    UI changes

---
+++ application/src/GUI/Controller/UserView/ticketViewController.java
@@ -0,0 +1,28 @@
+package GUI.Controller.UserView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ticketViewController implements Initializable {
+    /**
+     * Denne klasse er ikke blevet kodet endnu.
+     * Før dette kan gøres kræver det at man kan udstede en billet til en user.
+     * Tror gerg ville tage sig af det, så sætter det til siden.
+     */
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO
+        //Udfyld listen med billetter til den user der er logget ind.
+    }
+
+    @FXML
+    private void clickDownload(ActionEvent actionEvent) {
+        //TODO
+        //Download det event som er blevet klikket (Dette kræver at man kan give en user en billet først)
+    }
+}
